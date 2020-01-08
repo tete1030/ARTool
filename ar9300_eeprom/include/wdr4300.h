1aabb441e9614b1b1c71216fb993062320a634d6
Texot <tete1030@gmail.com>
2020-01-08 22:52:00 +0800

    Add my own data before fixup

--- ar9300_eeprom/include/wdr4300.h
+++ ar9300_eeprom/include/wdr4300.h
@@ -129,12 +129,38 @@ const static struct default_detect_data wdr4310_v1_0_my_data[AR9300_MAX_EEPROMS]
     },
 };
 
+const static struct ar9300_fixup wdr4310_v1_0_my_fixup = {
+    "Fix up calibration incomplete 5GHz, only works between 5745[149] and 5825[165]. Warning: fix up used inaccurate values (interpolated). Version 1.",
+
+    true,
+    { FREQ2FBIN(2412, 1), FREQ2FBIN(2437, 1), FREQ2FBIN(2462, 1) },
+    {
+        { { 11, 0, 157, 0, 0, 0 }, { 11, 0, 158, 0, 0, 0 }, { 10, 0, 158, 0, 0, 0 } }, 
+        { { 12, 0, 162, 0, 0, 0 }, { 13, 0, 162, 0, 0, 0 }, { 13, 0, 162, 0, 0, 0 } },
+        { {  0, 0,   0, 0, 0, 0 }, {  0, 0,   0, 0, 0, 0 }, {  0, 0,   0, 0, 0, 0 } }
+    },
+
+    true,
+    {
+        FREQ2FBIN(5745, 0), FREQ2FBIN(5825, 0), FREQ2FBIN(5320, 0), FREQ2FBIN(5400, 0),
+        FREQ2FBIN(5500, 0), FREQ2FBIN(5600, 0), FREQ2FBIN(5725, 0), FREQ2FBIN(5825, 0)
+    },
+    {
+        { { -6, 0, 156, 0, 0, 0 }, { -11, 0, 157, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 } },
+        { { -9, 0, 157, 0, 0, 0 }, { -15, 0, 157, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 } },
+        { { -10, 0, 157, 0, 0, 0 }, { -14, 0, 157, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 } }
+    }
+};
+
 const static struct ar9300_revision wdr4310_v1_0_2050500271_rev1_3 = {
     "TL-WDR4310", "1.0", "2050500271", "1.3", &wdr4310_v1_0_2050500271_rev1_3_fixup, default_detect, &wdr4310_v1_0_2050500271_rev1_3_data
 };
 
 const static struct ar9300_revision wdr4310_v1_0_my = {
-    "TL-WDR4310", "1.0", "my", "N/A", &wdr4310_v1_0_2050500271_rev1_3_fixup, default_detect, &wdr4310_v1_0_my_data
+    "TL-WDR4310", "1.0", "my", "N/A", &wdr4310_v1_0_my_fixup, default_detect, &wdr4310_v1_0_my_data
 };
 
 const static struct ar9300_detect wdr4300_detect = {
